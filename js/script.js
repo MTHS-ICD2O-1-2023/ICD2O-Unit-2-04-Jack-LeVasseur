91e02d1b6a78316fb8697cd70d8f867d80f9a16e
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-03-26 18:40:24 +0000

    Updated index.html Updated style.css Updated script.js Added image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack LeVasseur
+// Created on: March 21 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculatePermeterAndAreaOfRectangle () {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('length-of-rectangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('width-of-rectangle').value)
+
+  // process
+  const areaOfTriangle = baseOfTriangle + heightOfTriangle + 17.5
+  const HeightOfTriangle = (baseOfTriangle + heightOfTriangle) + 17.5 
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeterOfRectangle + ' cm'
+}
